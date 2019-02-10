a522a177910399c3b45b0558dcb6cab2944fc91c
IMOUNGA <imounga.r@gmail.com>
2019-02-10 02:12:17 +0100

    Mise en place Menu sans style

---
+++ src/templates/menu.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * create: Romann IMOUNGA
+ * Date: 10/02/19
+ * Time: 01:33
+ * But : Menu du site
+ */
+?>
+<Doctype html>
+    <head>
+        <meta charset="utf-8">
+        <title>Fitness-Web</title>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container" style="background: red">
+            <div class="row">
+                <div class="col-lg-6 col-md-4 col-sm-6" style="border:1px solid green;">
+                    <h1>Fitness Web</h1>
+                </div>
+                <div class="col-lg-6 col-md-4 col-sm-6" style="border:1px solid yellow;">
+                    <ul>
+                        <li>Concept</li>
+                        <li>Clubs</li>
+                        <li>Activités</li>
+                        <li>Actus</li>
+                        <li>Fitness App</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+    <!--
+        Il existe deux types de container
+            - container : qui est a largeur fixe
+            - container-fluid : qui prend toute la largeur
+
+        Les différentes types de container dépendent de la taille du viexport :
+            - xs < 768px  en général -> phones              prendra toute la largeur
+            - sm >= 768px en général -> tablettes           prendra 750px
+            - md >= 992px en général -> desktop             prendra 970px
+            - lg >= 1200px en général -> large desktop      prendra 1170px
+    -->
+</Doctype>
+
+
