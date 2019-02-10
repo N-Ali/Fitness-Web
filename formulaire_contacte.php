2fe179682dde76546b9c9acdb638750f56f0f3b4
Naushad-Ali Sookun <Naushad-AliSOOKUN@MacBook-Pro-de-Naushad-Ali.local>
2019-02-10 02:13:56 +0100

    Creation du formulaire test1

---
+++ formulaire_contacte.php
@@ -0,0 +1,36 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Inscription</title>
+        <meta charset="utf-8"/>
+
+    </head>
+
+    <body>
+      <div class ="Paragraphe_inscription">
+        <h1>Inscription en ligne</h1>
+            <p>Rejoignez notre club de sport</p>
+
+      </div>
+
+
+      <!-- div qui va englober tout le formulaire -->
+    <div class="formulaire">
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+  </body>
+
+
+</html>
