12f07a1348fd74bcf7fb6fea6cce2048e3539f31
N-Ali <Naushad-AliSOOKUN@MacBook-Pro-de-Naushad-Ali.local>
2019-02-12 00:21:25 +0100

    ajout cryptage du mdp mais ne fonctionne pas

--- inscription.php
+++ inscription.php
@@ -10,10 +10,13 @@ catch(Exception $e){
     die('ERROR :'.$e->getMessage());
 }
 
+$pass_hache = password_hash($_POST['Mot_de_passe'], PASSWORD_DEFAULT);
+$pass_hache2 = password_hash($_POST['Confirm_Mot_de_passe'], PASSWORD_DEFAULT);
+
 //Insertion des donnnes dans la bdd via une requete SQL//
 
 $reqs = $bdd->prepare('INSERT INTO  membres(Civilite,Nom,Prenom,Telephone,Email,Confirmation,Mot_de_passe,Confirm_Mot_de_passe,Adresse,Code_postal,Ville) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
-$reqs->execute(array($_POST['Civilite'], $_POST['Nom'], $_POST['Prenom'], $_POST['Telephone'], $_POST['Email'], $_POST['Confirmation'], $_POST['Mot_de_passe'], $_POST['Confirm_Mot_de_passe'], $_POST['Adresse'], $_POST['Code_postal'],$_POST['Ville']));
+$reqs->execute(array($_POST['Civilite'], $_POST['Nom'], $_POST['Prenom'], $_POST['Telephone'], $_POST['Email'], $_POST['Confirmation'], $pass_hache = $_POST['Mot_de_passe'], $pass_hache2 = $_POST['Confirm_Mot_de_passe'], $_POST['Adresse'], $_POST['Code_postal'],$_POST['Ville']));
 
-header('Location: menu.php');
+header('Location: src/templates/menu.php');
 ?>
