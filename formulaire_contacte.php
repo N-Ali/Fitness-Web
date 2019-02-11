c976ddbcb7b9eed63c6cc85c2f0f6992b24da41e
N-Ali <Naushad-AliSOOKUN@MacBook-Pro-de-Naushad-Ali.local>
2019-02-11 21:49:47 +0100

    Bug bdddans phpmyadmin ne prend pas en compte les information transmise sur le formulaire

--- formulaire_contacte.php
+++ formulaire_contacte.php
@@ -16,11 +16,11 @@
 
       <!-- div qui va englober tout le formulaire -->
     <div class="formulaire">
-        <form action="" method="post">
+        <form action="inscription.php" method="post">
 
             <!-- creation du champ civilité-->
             <div class = "forme">
-                <label class="forme_label">Civilité</label>
+                <label for="Civilite">Civilité</label>
                     <div class ="choix_civilite">
                         <select name="Civilite">
                             <option value="Monsieur">Monsieur</option>
@@ -31,52 +31,52 @@
 
             <!-- creation du champ Nom-->
             <div class="forme">
-                <label class="forme_label">Nom</label>
+                <label for="Nom">Nom</label>
                     <div class="champ">
                         <input type="text" name="Nom" id="Nom">
                     </div>
             </div>
             <!-- creation du champs prenom-->
             <div class="forme">
-                <label class="forme_label">Prenom</label>
+                <label for="Prenom">Prenom</label>
                 <input type="text" name="Prenom" id="Prenom">
             </div>
 
             <!-- creation du champs telephone-->
             <div class="forme">
-                <label class="forme_label">Télephone</label>
+                <label for="Telephone">Télephone</label>
                 <input type="text" name="Telephone" id="Telephone">
 
             </div>
 
             <!--creation du champ email-->
             <div class="forme">
-                <label class="forme_label">E-mail</label>
+                <label for="Email">E-mail</label>
                 <input type="text" name="Email" id="Email">
 
             </div>
 
             <!-- creation du champ confirmation email-->
             <div class ="forme">
-                <label class="forme_label">Confirmation de l'email</label>
+                <label for="Confirmation">Confirmation de l'email</label>
                 <input type="text" name="Confirmation" id="Confirmation">
             </div>
 
             <!--creation du champs adresse-->
             <div class="forme">
-                <label class="forme_label">Adresse</label>
+                <label for="Adresse">Adresse</label>
                 <input type="text" name="Adresse" id="Adresse">
             </div>
 
             <!-- creation du champs code postal-->
             <div class="forme">
-                <label class="forme_label">Code Postal</label>
+                <label for="Code_postal">Code Postal</label>
                 <input type="text" name="Code_postal" id="Code_postal">
             </div>
 
             <!-- creation du champs ville-->
             <div class="forme">
-                <label class="forme_label">Ville</label>
+                <label for="Ville">Ville</label>
                 <input type="text" name="Ville" id="Ville">
             </div>
 
